2d6e6cc31739417aee2787d071986cefc4ccdbc6
Piotr Jagiellowicz <Piotr.Jagiellowicz@gds.ey.com>
2020-01-10 22:34:48 +0100

    finished book routes

--- routes/books.js
+++ routes/books.js
@@ -46,13 +46,83 @@ router.post('/', async (req, res) => {
 
     try {
         const newBook = await book.save()
-        res.redirect('books')
+        res.redirect(`books/${newBook.id}`)
     } catch (error) {
         renderNewPage(res, book, true)
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+                                        .populate('author')
+                                        .exec()
+        res.render('books/show', { book: book })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req,res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        renderEditPage(res, book)
+    } catch (error) {
+        res.redirect('/')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let book
+
+    try {
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title
+        book.author = req.body.author
+        book.publishDate = new Date(req.body.publishDate)
+        book.pageCount = req.body.pageCount
+        book.description = req.body.description
+        if (req.body.cover != null && req.body.cover != '') {
+            saveCover(book, req.body.cover)
+        }
+        res.redirect(`/books/${book.id}`)
+    } catch (error) {
+        if (book != null) {
+            renderEditPage(res, book, true)
+        } else {
+            redirect('/')
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        await book.remove()
+        res.redirect('/books')
+    } catch (error) {
+        if (book != null) {
+            res.render('books/show', {
+                book: book,
+                errorMessage: 'Could not remove book'
+            })
+        } else {
+            res.redirect('/')
+        }
+    }
+})
+
 async function renderNewPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'edit', hasError)
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
     try {
         const authors = await Author.find({})
         const params = {
@@ -62,7 +132,7 @@ async function renderNewPage(res, book, hasError = false) {
         if (hasError) {
             params.errorMessage = "Error Creating Book"
         }
-        res.render('books/new', params)
+        res.render(`books/${form}`, params)
     } catch (error) {
         res.redirect('/books')
     }
